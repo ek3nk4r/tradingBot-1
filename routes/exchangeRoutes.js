da9c8e97c0288387c413f14b929980cf2b079487
bleepblopblorpblopbloop <kdchorley@hotmail.com>
2021-03-17 13:55:08 +0100

    gets correct user data from encrypted keys

--- routes/exchangeRoutes.js
+++ routes/exchangeRoutes.js
@@ -1,17 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
-const exchange = require("../exchangeObjects/exchangeObjects");
-const { userKeys } = require("../exchangeObjects/exchangeObjects");
-
-const bybit = exchange.bybit;
-const bitmex = exchange.bitmex;
-const phemex = exchange.phemex;
-const exchanges = [bybit, bitmex, phemex];
-
-bybit.urls["api"] = bybit.urls["test"];
-bitmex.urls["api"] = bitmex.urls["test"];
-phemex.urls["api"] = phemex.urls["test"];
+const ccxt = require("ccxt");
+const User = require("../models/User");
+const ExchangeAccount = require("../models/ExchangeAccount");
+const { decrypt } = require("../crypto/crypto");
 
 let exchangeObject;
 
@@ -20,31 +13,52 @@ router.get("/tickers/:exchangeName/:userId", (req, res) => {
   const userId = req.params.userId;
   console.log("***EXCHANGE NAME***", exchangeName, userId);
 
-  userKeys(exchangeName, userId);
+  User.findById(userId)
+    .populate("exchangeAccount")
+    .then((userInfo) => {
+      const [exhangeAccountIdToPopulate] = userInfo.exchangeAccount.filter(
+        (el) => {
+          if (el.exchangeName.toLowerCase() == exchangeName) {
+            return el._id;
+          }
+        }
+      );
+      ExchangeAccount.findById(exhangeAccountIdToPopulate)
+        .populate("secret")
+        .then((secretInfo) => {
+          let key = secretInfo.key;
+          let hash = secretInfo.secret[0];
 
-  const exchangeFilter = exchanges.filter((exchange) => {
-    if (exchange.constructor.name == exchangeName) {
-      exchangeObject = exchange;
-    }
-  });
+          exchangeObject = new ccxt[exchangeName]({
+            apiKey: key,
+            secret: decrypt(hash),
+            timeout: 5000,
+            enableRateLimit: true,
+          });
 
-  (async function () {
-    try {
-      const exchangeInfo = await exchangeObject.has;
-      const markets = await exchangeObject.load_markets();
-      let tickers;
-      if (exchangeName == "phemex") {
-        tickers = await exchangeObject.publicGetCfgV2Products();
-      } else {
-        tickers = await exchangeObject.fetchTickers();
-      }
-      const exchangeData = [exchangeInfo, markets, tickers];
-      res.json(exchangeData);
-    } catch (err) {
-      console.error(err);
-      return {};
-    }
-  })();
+          exchangeObject.urls["api"] = exchangeObject.urls["test"];
+
+          (async function () {
+            try {
+              const exchangeInfo = await exchangeObject.has;
+              const markets = await exchangeObject.load_markets();
+              let tickers;
+              if (exchangeName == "phemex") {
+                tickers = await exchangeObject.publicGetCfgV2Products();
+              } else {
+                tickers = await exchangeObject.fetchTickers();
+              }
+              const exchangeData = [exchangeInfo, markets, tickers];
+              res.json(exchangeData);
+            } catch (err) {
+              console.error(err);
+              return {};
+            }
+          })();
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
 });
 
 router.post("/coinData", (req, res) => {
--- routes/tradingRoutes.js
+++ routes/tradingRoutes.js
@@ -1,57 +1,71 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
-const exchange = require("../exchangeObjects/exchangeObjects");
+const ccxt = require("ccxt");
+const User = require("../models/User");
+const ExchangeAccount = require("../models/ExchangeAccount");
+const { decrypt } = require("../crypto/crypto");
 const { buy } = require("../tradeExecution/buy");
 const { closeBuy } = require("../tradeExecution/closeBuy");
 const { sell } = require("../tradeExecution/sell");
 const { closeSell } = require("../tradeExecution/closeSell");
 
-const bybit = exchange.bybit;
-const bitmex = exchange.bitmex;
-const phemex = exchange.phemex;
-const exchanges = [bybit, bitmex, phemex];
-
-bybit.urls["api"] = bybit.urls["test"];
-bitmex.urls["api"] = bitmex.urls["test"];
-phemex.urls["api"] = phemex.urls["test"];
-
-let exchangeObject;
-
 router.post("/tradingRoutes", (req, res) => {
   console.log("WEBHOOK RECEIVED:", req.body.text);
 
   const exchangeName = req.body.text.exchange;
-
-  const exchangeFilter = exchanges.filter((exchange) => {
-    if (exchange.constructor.name == exchangeName) {
-      exchangeObject = exchange;
-    }
-  });
-
+  const userId = req.params.userId;
   const webHook = req.body.text;
   const instrument = webHook.instrument;
   const coin = instrument.slice(0, instrument.indexOf("/"));
   res.json(webHook);
   res.status(200).end();
 
-  if (webHook.alert_message === "buy") {
-    (async function () {
-      buy(exchangeObject, exchangeName, webHook, instrument, coin);
-    })();
-  } else if (webHook.alert_message === "sell") {
-    (async function () {
-      sell(exchangeObject, exchangeName, webHook, instrument, coin);
-    })();
-  } else if (webHook.alert_message === "close buy") {
-    (async function () {
-      closeBuy(exchangeObject, exchangeName, instrument);
-    })();
-  } else if (webHook.alert_message === "close sell") {
-    (async function () {
-      closeSell(exchangeObject, exchangeName, instrument);
-    })();
-  }
+  User.findById(userId)
+    .populate("exchangeAccount")
+    .then((userInfo) => {
+      const [exhangeAccountIdToPopulate] = userInfo.exchangeAccount.filter(
+        (el) => {
+          if (el.exchangeName.toLowerCase() == exchangeName) {
+            return el._id;
+          }
+        }
+      );
+      ExchangeAccount.findById(exhangeAccountIdToPopulate)
+        .populate("secret")
+        .then((secretInfo) => {
+          let key = secretInfo.key;
+          let hash = secretInfo.secret[0];
+          exchangeObject = new ccxt[exchangeName]({
+            apiKey: key,
+            secret: decrypt(hash),
+            timeout: 5000,
+            enableRateLimit: true,
+          });
+
+          exchangeObject.urls["api"] = exchangeObject.urls["test"];
+
+          if (webHook.alert_message === "buy") {
+            (async function () {
+              buy(exchangeObject, exchangeName, webHook, instrument, coin);
+            })();
+          } else if (webHook.alert_message === "sell") {
+            (async function () {
+              sell(exchangeObject, exchangeName, webHook, instrument, coin);
+            })();
+          } else if (webHook.alert_message === "close buy") {
+            (async function () {
+              closeBuy(exchangeObject, exchangeName, instrument);
+            })();
+          } else if (webHook.alert_message === "close sell") {
+            (async function () {
+              closeSell(exchangeObject, exchangeName, instrument);
+            })();
+          }
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
 });
 
 module.exports = router;
