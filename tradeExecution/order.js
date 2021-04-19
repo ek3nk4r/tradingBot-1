d6aa17bdd82dc37a20cc03ab648f38e6460dc71a
bleepblopblorpblopbloop <kdchorley@hotmail.com>
2021-04-19 14:18:44 +0200

    bybit can set leverage - phemex stops not working

--- tradeExecution/order.js
+++ tradeExecution/order.js
@@ -8,6 +8,7 @@ const order = async (
   try {
     let balance;
     if (exchangeName == "phemex") {
+      let params;
       balance = await exchangeObject.fetchBalance(
         (params = { type: "swap", code: coin })
       );
@@ -29,26 +30,69 @@ const order = async (
     }
 
     let params;
-    if (webHook.stopOrder === true && webHook.takeProfit === false) {
-      params = {
-        stop_loss: webHook.stopPrice,
-      };
-    } else if (webHook.takeProfit === true && webHook.stopOrder === false) {
-      params = {
-        take_profit: webHook.profitPrice,
-      };
-    } else if (webHook.stopOrder === true && webHook.takeProfit === true) {
-      params = {
-        stop_loss: webHook.stopPrice,
-        take_profit: webHook.profitPrice,
-      };
-    } else {
-      params = {};
+    switch (exchangeObject) {
+      case "bybit":
+        if (webHook.stopOrder === true && webHook.takeProfit === false) {
+          params = {
+            stop_loss: webHook.stopPrice,
+          };
+        } else if (webHook.takeProfit === true && webHook.stopOrder === false) {
+          params = {
+            take_profit: webHook.profitPrice,
+          };
+        } else if (webHook.stopOrder === true && webHook.takeProfit === true) {
+          params = {
+            stop_loss: webHook.stopPrice,
+            take_profit: webHook.profitPrice,
+          };
+        } else {
+          params = {};
+        }
+        break;
+      // case "phemex":
+      //   if (webHook.stopOrder === true && webHook.takeProfit === false) {
+      //     params = {
+      //       ordType: "Stop",
+      //       // priceEp: exchange.to_ep(webHook.stopPrice, instrument),
+      //       stopPx: webHook.stopPrice,
+      //       // stopLossEp: exchange.to_ep(webHook.stopPrice, instrument),
+      //       // triggerType: webHook.triggerType,
+      //     };
+      //   } else if (webHook.takeProfit === true && webHook.stopOrder === false) {
+      //     params = {
+      //       ordType: webHook.orderType,
+      //       takeProfitEp: exchange.to_ep(webHook.profitPrice, instrument),
+      //     };
+      //   } else if (webHook.stopOrder === true && webHook.takeProfit === true) {
+      //     params = {
+      //       // stopPxEp: exchange.to_ep(webHook.triggerPrice, instrument),
+      //       takeProfitEp: exchange.to_ep(webHook.profitPrice, instrument),
+      //       stopLossEp: exchange.to_ep(webHook.stopPrice, instrument),
+      //       // triggerType: webHook.triggerType,
+      //     };
+      //   } else {
+      //     params = {};
+      //   }
+      //   break;
     }
 
     const side = webHook.side;
     const limitPrice = webHook.limitPrice;
     const type = webHook.orderType;
+    const market = exchangeObject.market(instrument);
+
+    if (exchangeName == "bybit") {
+      const executions = await exchangeObject.v2PrivateGetTradeClosedPnlList({
+        symbol: market["id"],
+      });
+
+      if (executions.result.data[0].leverage != webHook.leverage) {
+        await exchangeObject.v2PrivatePostPositionLeverageSave({
+          symbol: instrument.replace("/", ""),
+          leverage: webHook.leverage,
+        });
+      }
+    }
 
     switch (webHook.orderType) {
       case "limit":
@@ -67,7 +111,6 @@ const order = async (
           type,
           side,
           amount,
-          limitPrice,
           params
         );
         break;
